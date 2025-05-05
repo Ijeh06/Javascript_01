f0e730f6316cbb85f24263405a1c2fd0aa08925c
ijeh ekene <ijehekene404@gmail.com>
2025-05-05 13:22:23 +0100

    comment out initial code for clarity and organization

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let js = 'Amazing';
-console.log( 40 + 30 + 30 -10);
+// let js = 'Amazing';
+// console.log( 40 + 30 + 30 -10);
 
-console.log('EKENE');
-console.log(49);
+// console.log('EKENE');
+// console.log(49);
 
-let firstName = 'Ekene';
-console.log(firstName);
+// let firstName = 'Ekene';
+// console.log(firstName);
 
-let Ekene_Babe ="EB";
-let $function = "27"
+// let Ekene_Babe ="EB";
+// let $function = "27"
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-let job1 ="programmer";
-let job2 = "teacher";
+// let job1 ="programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob, myCurrentJob);
+
+true; 
+console.log(true); 
