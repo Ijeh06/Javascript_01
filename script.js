a42df27f41a0101d1741ce029892903df1d2aaea
Ekene Ijeh <ijehekene404@gmail.com>
2025-06-09 10:51:17 +0100

    update type conversion examples and add type coercion demonstrations

--- script.js
+++ script.js
@@ -191,10 +191,17 @@
 //     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 // }
 
-// //Type Conversion and Coercion
 
+// type conversion 
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear); //1991
 console.log(Number(inputYear) + 18); //199118
 
 console.log(Number('Ekene')); //NaN
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
