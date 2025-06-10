0823307ededf56868747b94569cc7b90ac99a732
Ekene Ijeh <ijehekene404@gmail.com>
2025-06-10 17:30:50 +0100

    feat: add type coercion examples to demonstrate string and number operations

--- script.js
+++ script.js
@@ -205,3 +205,8 @@ console.log(String(23), 23);
 //type coercion
 console.log('I am ' + 23 + ' years old');
 console.log('23' - '10' - 3);
+console.log('23' * '2');
+
+let n = '1' + 1; 
+n = n - 1; 
+console.log(n); 
